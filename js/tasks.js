a7fc0eb3b503c93b704914dba37d534d7c3640a2
Nataliya Pavlenko <mrsnataliya@gmail.com>
2020-08-14 01:21:01 +0300

    add task-01

--- js/tasks.js
+++ js/tasks.js
@@ -1,101 +1,101 @@
 import users from "./users.js";
 
 /*01*/
-/* Получить массив имен всех пользователей(поле name).*/
+/* Получить массив имен всех пользователей  (поле name).*/
 
 const getUserNames = (users) => {
-  // твой код
+  return users.map((user) => user.name);
 };
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-/*02*/
-/* Получить массив объектов пользователей по цвету глаз(поле eyeColor).*/
+// /*02*/
+// /* Получить массив объектов пользователей по цвету глаз(поле eyeColor).*/
 
-const getUsersWithEyeColor = (users, color) => {
-  // твой код
-};
+// const getUsersWithEyeColor = (users, color) => {
+//   // твой код
+// };
 
-console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+// console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-/*03*/
-/* Получить массив имен пользователей по полу(поле gender).*/
+// /*03*/
+// /* Получить массив имен пользователей по полу(поле gender).*/
 
-const getUsersWithGender = (users, gender) => {
-  // твой код
-};
+// const getUsersWithGender = (users, gender) => {
+//   // твой код
+// };
 
-console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+// console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-/*04*/
-/* Получить массив только неактивных пользователей(поле isActive).*/
+// /*04*/
+// /* Получить массив только неактивных пользователей(поле isActive).*/
 
-const getInactiveUsers = (users) => {
-  // твой код
-};
+// const getInactiveUsers = (users) => {
+//   // твой код
+// };
 
-console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-/*05*/
-/* Получить пользоваля (не массив) по email(поле email, он уникальный). */
+// /*05*/
+// /* Получить пользоваля (не массив) по email(поле email, он уникальный). */
 
-const getUserWithEmail = (users, email) => {
-  // твой код
-};
+// const getUserWithEmail = (users, email) => {
+//   // твой код
+// };
 
-console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+// console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+// console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
 
-/*06*/
-/* Получить массив пользователей попадающих в возрастную категорию от min до max лет(поле age).*/
+// /*06*/
+// /* Получить массив пользователей попадающих в возрастную категорию от min до max лет(поле age).*/
 
-const getUsersWithAge = (users, min, max) => {
-  // твой код
-};
+// const getUsersWithAge = (users, min, max) => {
+//   // твой код
+// };
 
-console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-console.log(getUsersWithAge(users, 30, 40));
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+// console.log(getUsersWithAge(users, 30, 40));
+// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-/*07*/
-/* Получить общую сумму баланса(поле balance) всех пользователей. */
+// /*07*/
+// /* Получить общую сумму баланса(поле balance) всех пользователей. */
 
-const calculateTotalBalance = (users) => {
-  // твой код
-};
+// const calculateTotalBalance = (users) => {
+//   // твой код
+// };
 
-console.log(calculateTotalBalance(users)); // 20916
+// console.log(calculateTotalBalance(users)); // 20916
 
-/*08*/
-/* Массив имен всех пользователей у которых есть друг с указанным именем. */
+// /*08*/
+// /* Массив имен всех пользователей у которых есть друг с указанным именем. */
 
-const getUsersWithFriend = (users, friendName) => {
-  // твой код
-};
+// const getUsersWithFriend = (users, friendName) => {
+//   // твой код
+// };
 
-console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
-/*09*/
-/* Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends) */
+// /*09*/
+// /* Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends) */
 
-const getNamesSortedByFriendsCount = (users) => {
-  // твой код
-};
+// const getNamesSortedByFriendsCount = (users) => {
+//   // твой код
+// };
 
-console.log(getNamesSortedByFriendsCount(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+// console.log(getNamesSortedByFriendsCount(users));
+// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-/*10*/
-/* Получить массив всех умений всех пользователей(поле skills), 
- при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.*/
+// /*10*/
+// /* Получить массив всех умений всех пользователей(поле skills),
+//  при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.*/
 
-const getSortedUniqueSkills = (users) => {
-  // твой код
-};
+// const getSortedUniqueSkills = (users) => {
+//   // твой код
+// };
 
-console.log(getSortedUniqueSkills(users));
-/* [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure',
-'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ] */
+// console.log(getSortedUniqueSkills(users));
+// /* [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure',
+// 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ] */
